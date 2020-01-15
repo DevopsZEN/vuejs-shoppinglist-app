ecee389200087650bce27a5bd5e38536a6f03762
Othmane Zenzamy <devops.zenzamy@gmail.com>
2020-01-15 19:47:56 +0100

    Adding more interactivity to our item when we click the item the text will be muted and vise-versa

--- main.js
+++ main.js
@@ -30,6 +30,9 @@ var shoppingList = new Vue({
         changeState: function(newState) {
             this.state = newState;
             this.newItem = '';
+        },
+        togglePurchased: function(item) {
+            item.purchased = !item.purchased;
         }
     }
 })
